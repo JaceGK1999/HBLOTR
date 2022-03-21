73a366766ad25f93007257b51bf3f9f8d3394daa
Jacegk1999 <Jacegk1999@gmail.com>
2022-03-21 16:29:42 -0700

    have the ability to switch to characters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 import Characters from './views/Characters/Characters';
+import Welcome from './views/Main/Main';
 
 function App() {
   return (
     <div className="App">
-      <Characters />
+      <BrowserRouter>
+        <Header />
+        <div className="App">
+          <Switch>
+            <Route exact path="/">
+              <Welcome />
+            </Route>
+            <Route path={'/characters'}>
+              <Characters />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
