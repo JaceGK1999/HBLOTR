d587304836d326f1e48db02a853a207cb6f9d937
Jacegk1999 <Jacegk1999@gmail.com>
2022-03-21 17:35:54 -0700

    have films rendering

--- src/views/Films/Films.js
+++ src/views/Films/Films.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { fetchFilms } from '../../services/films';
 
 export default function Films() {
-  return <div>hi</div>;
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    const fetchFilmData = async () => {
+      const data = await fetchFilms();
+      console.log(data);
+      setFilms(data);
+    };
+    fetchFilmData();
+  }, []);
+
+  return (
+    <div>
+      <h3>This is all of the films!!</h3>
+      {films.map((item) => (
+        <p key={item.id}>{item.title}</p>
+      ))}
+    </div>
+  );
 }
